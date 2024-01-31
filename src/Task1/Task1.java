438715a1f5813da12c02ac73f9e0706aa0cf28cd
LunguIon <lion.24072004@gmail.com>
2024-01-31 19:08:07 +0200

    Done task 1

---
+++ src/Task1/Task1.java
@@ -0,0 +1,12 @@
+package Task1;
+
+import javax.swing.*;
+
+public class Task1 {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Wow:)");
+        jf.setSize(400, 300);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+    }
+}
